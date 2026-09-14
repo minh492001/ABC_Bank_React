b69bfd12ccdf8937679c5f7be526567ba54358d0
MinMin <gow.bin.gg@gmail.com>
2026-09-14 18:35:03 +0700

    feat: add Profile page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from "./pages/register";
 import Login from "./pages/login";
 import Home from "./pages/home";
 import NotFound from "./pages/notFound";
+import Profile from "./pages/profile";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+
 
         {/* 404 Not Found error */}
         <Route path="*" element={<NotFound/>}/>
